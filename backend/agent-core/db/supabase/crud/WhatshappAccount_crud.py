2ebd95307022544e5bdef272a6918e137d895fff
Keshav raj <itsrajkeshav@gmail.com>
2026-02-21 10:58:33 +0530

    feat: add WhatshappAccount model and CRUD operations

---
+++ backend/agent-core/db/supabase/crud/WhatshappAccount_crud.py
@@ -0,0 +1,49 @@
+from sqlalchemy.orm import Session
+from ..models.whatshappAccount import WhatshappAccount
+from datetime import datetime
+
+def create_whatshapp_account(db:Session, user_id:str, phone_number:str, jid:str):
+    account_creation = WhatshappAccount(
+        user_id=user_id,
+        phone_number=phone_number,
+        jid=jid,
+        status="active",
+        created_at=datetime.utcnow(),
+        updated_at=datetime.utcnow(),
+    )
+    db.add(account_creation)
+    db.commit()
+    db.refresh(account_creation)
+    return {
+        "status": "success",
+        "message": "Whatshapp account created successfully",
+        "account": account_creation
+    }
+
+def get_whatshapp_account(db:Session, user_id:str):
+    account = db.query(WhatshappAccount).filter(WhatshappAccount.user_id == user_id).first()
+    if account:
+        return {
+            "status": "success",
+            "message": "Whatshapp account found",
+            "account": account
+        }
+    return {
+        "status": "error",
+        "message": "Whatshapp account not found",
+    }
+def update_whatshapp_account(db:Session,user_id:str, phone_number:str, jid:str, status:str):
+    account_update = get_whatshapp_account(db, user_id)
+    if account_update:
+        account_update.updated_at = datetime.utcnow()
+        db.commit()
+        db.refresh(account_update)
+        return {
+            "status": "success",
+            "message": "Whatshapp account updated successfully",
+            "account": account_update
+        }
+    return {
+        "status": "error",
+        "message": "Whatshapp account not found",
+    }
---
+++ backend/agent-core/db/supabase/models/whatshappAccount.py
@@ -0,0 +1,17 @@
+import string
+from sqlalchemy import Column, column,Integer,String,DateTime, Enum as SQLEnum
+from datetime import datetime, timezone
+from db.supabase.connectDB import Base
+
+class WhatshappAccount(Base):
+    __tablename__ = "whatshapp_accounts"
+
+    id = Column(Integer , autoincrement=True)
+    user_id = Column(String, nullable=False, primary_key=True, index=True)
+    phone_number = Column(String, nullable=False, index=True, unique=True)
+    jid =Column(String, nullable=False, index=True, unique=True)
+    status = Column(SQLEnum("active", "inactive", name="status_enum"), nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    def __repr__(self):
+        return f"<WhatshappAccount(user_id={self.user_id}, phone_number={self.phone_number}, jid={self.jid})>"
